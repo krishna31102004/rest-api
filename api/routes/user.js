8836ea9fa28f2edf5e20e6cd9bbdc166a58fc371
Krishna Balaji <krishna311004@gmail.com>
2024-11-06 21:48:13 -0700

    push code

---
+++ api/routes/user.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const checkAuth = require('../middleware/check-auth');
+
+const User = require('../models/user');
+const UserController = require('../controllers/user');
+
+router.post('/signup', UserController.user_signup);
+
+router.post('/login', UserController.user_login);
+
+router.delete('/:userId', checkAuth,UserController.user_delete);
+
+module.exports = router;
